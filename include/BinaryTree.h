714942f4e9f672aaadfc986741822811f8e63de8
Semyon Kulish <sem_kulish@mail.ru>
2018-10-28 18:18:41 +0300

    Interface of AVL tree and infra update

--- include/BinaryTree.h
+++ include/BinaryTree.h
@@ -6,12 +6,12 @@
 template <class T>
 class BinaryTree{
 protected:
-    Node *root;
-    virtual Node* createNode(const T &key) = 0;
-    virtual void deleteNode(Node *&node) = 0;
+    Node<T> *root;
+    virtual Node<T>* createNode(const T &key) = 0;
+    virtual void deleteNode(Node<T> *&node) = 0;
 
 public:
-    BinaryTree(){root = nullptr};
+    BinaryTree(){root = nullptr;};
     virtual void insertRecord(T &key) = 0;
     virtual void getRecord(T &key) = 0;
 };
